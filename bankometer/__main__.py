0ae67bae509aa75ef26ea23e4cc541e95510893e
Stefan Nožinić <stefan@lugons.org>
2025-07-05 19:29:27 +0200

    Restrict REST API serving to specific environment variable value

--- bankometer/__main__.py
+++ bankometer/__main__.py
@@ -5,7 +5,7 @@ from orgasm.http_rest import serve_rest_api
 import os 
 
 def main():
-    if "BANKOMETER_REST_API" in os.environ:
+    if "BANKOMETER_REST_API" in os.environ and os.environ["BANKOMETER_REST_API"] == "1":
         serve_rest_api([Methods], 8000, "0.0.0.0")
         return
     command_executor_main(Methods, explicit_params=True)
