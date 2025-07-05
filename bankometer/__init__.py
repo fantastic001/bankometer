a20c707ce2a7de5e6a318b227019ecc13c6577da
Stefan Nožinić <stefan@lugons.org>
2025-07-05 04:01:18 +0200

    Add Dockerfile and docker-compose for deployment; update requirements to include Flask

--- bankometer/__init__.py
+++ bankometer/__init__.py
@@ -10,24 +10,78 @@ import sqlalchemy.orm
 import sqlalchemy.orm.dynamic
 
 from bankometer import objdiff
-from bankometer.config import get_config_dict
+from bankometer.config import get_config_dict, get_config_str
+from orgasm.http_rest import http_get, http_post
 
+
+DEFAULT_GNUCASH_FILE = get_config_str(
+    "gnucash_file", 
+    "bankometer.gnucash",
+    "Path to gnucash file, used for storing transactions and accounts"
+)
 
 def getattrs(obj):
     return {
         a: getattr(obj, a) for a in dir(obj) if not a.startswith("__") and not callable(getattr(obj, a))
     }
+
+def get_aliases():
+    return get_config_dict("account_aliases", {}, "Aliases for accounts in gnucash file")
+
 def get_account(account):
-    aliases = get_config_dict("account_aliases", {}, "Aliases for accounts in gnucash file")
+    aliases = get_aliases()
     if account in aliases:
         return aliases[account]
     return account
+
+def is_child(parent, child):
+    """
+    Check if parent is a parent of child in gnucash account hierarchy.
+
+    Example:
+        is_child("Assets:Bank:MyBank", "Assets:Bank:MyBank:Checking")
+
+
+    Args:
+        parent (str): Full name of the parent account.
+        child (str): Full name of the child account.
+    Returns:
+        bool: True if parent is a parent of child, False otherwise.
+    """
+    if not parent or not child:
+        return False
+    if parent == child:
+        return True
+    p = parent.split(":")
+    c = child.split(":")
+    if len(p) >= len(c):
+        return False
+    for i in range(len(p)):
+        if p[i] != c[i]:
+            return False
+    return True
+
+
 class Methods:
     
-    def accounts(self, gnucash_file: str):
+    def aliases(self):
+        """
+        Returns aliases for accounts in gnucash file.
+        """
+        aliases = get_aliases()
+        data = []
+        for alias, account in aliases.items():
+            data.append({
+                "alias": alias,
+                "account": account
+            })
+        return data
+
+    def accounts(self):
         """
         Returns accounts from gnucash file.
         """
+        gnucash_file = DEFAULT_GNUCASH_FILE
         book = piecash.open_book(gnucash_file)
         data = [] 
         for account in book.accounts:
@@ -44,7 +98,9 @@ class Methods:
             })
         return data 
 
-    def transactions(self, gnucash_file: str, *, account: str = ""):
+    @http_get
+    def transactions(self, *, account: str = ""):
+        gnucash_file = DEFAULT_GNUCASH_FILE
         account = get_account(account)
         book = piecash.open_book(gnucash_file)
         transactions = book.transactions
@@ -65,10 +121,12 @@ class Methods:
             })
         return sorted(data, key=lambda x: x["post_date"], reverse=False)
 
-    def balances(self, gnucash_file: str, *, traditional: bool = False):
+    @http_get
+    def balances(self, *, traditional: bool = False):
         """
         Returns balances of all accounts in gnucash file.
         """
+        gnucash_file = DEFAULT_GNUCASH_FILE
         if traditional:
             from bankometer.gnucash import GnuCashBook
             book = GnuCashBook.open_book(gnucash_file)
@@ -80,11 +138,11 @@ class Methods:
                     "balance": account.get_balance()
                 })
             return data
-        accounts = self.accounts(gnucash_file)
+        accounts = self.accounts()
         data = [] 
         for account in accounts:
             balance = 0 
-            for transaction in self.transactions(gnucash_file, account=account["name"]):
+            for transaction in self.transactions(account=account["name"]):
                 if transaction["account_amount"] is not None:
                     balance += transaction["account_amount"]
             data.append({
@@ -93,8 +151,9 @@ class Methods:
             })
         return data
 
-    def add_transaction(self, gnucash_file: str, source: str, target: str, 
+    def add_transaction(self, source: str, target: str,
             amount: float, description: str, *, currency: str = "RSD"):
+        gnucash_file = DEFAULT_GNUCASH_FILE
         destination = target
         old_balance = self.balances(gnucash_file)
         amount: Decimal = Decimal("%f" % amount)
@@ -125,3 +184,5 @@ class Methods:
         new_balance = self.balances(gnucash_file)
         balance_diff = objdiff.diff(old_balance, new_balance)
         return balance_diff
+
+    
--- bankometer/__main__.py
+++ bankometer/__main__.py
@@ -1,10 +1,14 @@
 
 from bankometer import Methods
 from orgasm import command_executor_main, get_classes
-
+from orgasm.http_rest import serve_rest_api
+import os 
 
 def main():
-    # main()
+    if "BANKOMETER_REST_API" in os.environ:
+        serve_rest_api([Methods], 8000, "0.0.0.0")
+        return
     command_executor_main(Methods, explicit_params=True)
+
 if __name__ == "__main__":
     main()
